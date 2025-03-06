4d3d3d56ac55264729c53ab57550d4e805da29e1
ajand <ajand@MaityAdeventure.home>
2025-03-06 20:07:59 +0000

    Hello git. First commit to test.

---
+++ src/main/java/distsys/juansmartcitysolution/JuanSmartCitySolution.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package distsys.juansmartcitysolution;
+
+/**
+ *
+ * @author ajand
+ */
+public class JuanSmartCitySolution {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
